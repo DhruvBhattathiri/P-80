f216662f1e2d0a81d7e1c7c2608794f2ae57fa3c
DhruvBhattathiri <73540837+DhruvBhattathiri@users.noreply.github.com>
2021-02-04 15:36:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function para(){
+    var input=[];
+ for(var i=1 ; i <=6 ; i++){
+     input.push(document.getElementById("a" +i).value);
+     input.join(". ");
+     document.getElementById("a").innerHTML = inputs.join(". ");
+ }
+ input.join(". ");
+ document.getElementById("a").innerHTML = inputs.join(". ");
+}
+function para1(){
+    var input=[];
+ for(var j=1 ; j <=6 ; i++){
+     input.push(document.getElementById("a" +j).value);
+     input.join(". ");
+     document.getElementById("b").innerHTML = inputs.join(". ");
+ }
+ input.join(". ");
+ document.getElementById("b").innerHTML = inputs.join(". ");
+}
